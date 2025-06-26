992f1fb669dbe239dc09e3eb3fbcc8541e075b9b
Yeison R <yeisonr@uninorte.edu.co>
2025-06-26 02:05:44 -0500

    fix: Corregido error de prop undefined en SellerCard
    - Movida la lógica de fetching al BodyLayout
    - Agregados PropTypes para validación
    - Mejorado el skeleton loading
    - Pasadas props correctamente a los componentes hijos

--- challengeyeison.client/src/components/common/BodyLayout.jsx
+++ challengeyeison.client/src/components/common/BodyLayout.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import "../../styles/meli.css";
 import ImageGallery from "../productDetail/ImageGallery";
 import ProductDetail from "../productDetail/ProductDetail";
@@ -6,19 +7,44 @@ import PriceInfo from "../productDetail/PriceInfo";
 import PaymentOptions from "../productDetail/PaymentOptions";
 import SellerInfo from "../productDetail/SellerInfo";
 import SellerCard from "../productDetail/SellerCard";
+import useProduct from "../../hooks/useProduct";
 
 const BodyLayout = () => {
+    const { id } = useParams();
+    const { product, loading, error } = useProduct(id);
+
+    if (loading) return (
+        <div className="max-w-[1200px] mx-auto p-4">
+            <div className="animate-pulse grid grid-cols-12 gap-4">
+                <div className="col-span-3 bg-gray-200 h-96 rounded-lg"></div>
+                <div className="col-span-6 bg-gray-200 h-96 rounded-lg"></div>
+                <div className="col-span-3 space-y-4">
+                    <div className="bg-gray-200 h-48 rounded-lg"></div>
+                    <div className="bg-gray-200 h-48 rounded-lg"></div>
+                </div>
+            </div>
+        </div>
+    );
+
+    if (error) return (
+        <div className="max-w-[1200px] mx-auto p-4 text-red-500">
+            Error al cargar el producto: {error}
+        </div>
+    );
+
+    if (!product) return null;
+
     return (
         <div className="max-w-[1200px] mx-auto p-4">
             <div className="grid grid-cols-12 gap-4">
                 {/* Columna izquierda - Galería */}
                 <div className="col-span-3 bg-white p-4 rounded-lg">
-                    <ImageGallery />
+                    <ImageGallery images={product.images} />
                 </div>
 
                 {/* Columna central - Información del producto */}
                 <div className="col-span-6 bg-white p-4 rounded-lg">
-                    <ProductDetail />
+                    <ProductDetail product={product} />
                 </div>
 
                 {/* Columna derecha - Información de compra y vendedor */}
@@ -26,14 +52,14 @@ const BodyLayout = () => {
                     {/* Sección de precios y compra */}
                     <div className="bg-white p-4 rounded-lg">
                         <div className="space-y-4">
-                            <PriceInfo />
-                            <PaymentOptions />
-                            <SellerInfo />
+                            <PriceInfo product={product} />
+                            <PaymentOptions payment={product.payment} />
+                            <SellerInfo seller={product.seller} />
                         </div>
                     </div>
 
                     {/* Tarjeta del vendedor */}
-                    <SellerCard />
+                    <SellerCard seller={product.seller} />
                 </div>
             </div>
         </div>
