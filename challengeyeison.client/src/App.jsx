2530a5f80ce6f4791979b6c5803e62f46a41553b
Yeison R <yeisonr@uninorte.edu.co>
2025-06-26 02:46:27 -0500

    fix: Corregir estructura de la aplicación y exportaciones

--- challengeyeison.client/src/App.jsx
+++ challengeyeison.client/src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from "./components/common/Header";
-import BodyLayout from "./components/common/BodyLayout";
 import ProductPage from './pages/ProductPage';
 import './App.css';
 
 export default function App() {
     return (
         <BrowserRouter>
-            <Header />
-            <BodyLayout />
-            <Routes>
-                {/* Redirigir la ruta raíz a un producto por defecto */}
-                <Route path="/" element={<Navigate to="/producto/MLB3025054960" replace />} />
-                <Route path="/producto/:id" element={<ProductPage />} />
-                {/* Ruta de fallback para IDs no válidos */}
-                <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
+            <div className="min-h-screen bg-gray-100">
+                <Header />
+                <Routes>
+                    {/* Redirigir la ruta raíz a un producto por defecto */}
+                    <Route path="/" element={<Navigate to="/producto/MLB3025054960" replace />} />
+                    <Route path="/producto/:id" element={<ProductPage />} />
+                    {/* Ruta de fallback para IDs no válidos */}
+                    <Route path="*" element={<Navigate to="/" replace />} />
+                </Routes>
+            </div>
         </BrowserRouter>
     );
 }
