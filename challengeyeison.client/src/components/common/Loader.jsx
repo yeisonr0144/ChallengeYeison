b3c58ada6e11b6e2dee75353bc7ccae594752e31
Yeison R <yeizon_18@hotmail.com>
2025-06-27 15:02:10 -0500

    feat: Configuración inicial de pruebas unitarias con Vitest

--- challengeyeison.client/src/components/common/Loader.jsx
+++ challengeyeison.client/src/components/common/Loader.jsx
@@ -1,3 +1,7 @@
 export default function Loader() {
-    return <div className="text-center py-8">Cargando producto...</div>;
+    return (
+        <div data-testid="loader" className="text-center py-8">
+            Cargando producto...
+        </div>
+    );
 }
